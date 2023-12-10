ea099acd1c7aaf55f37aa20b3a1c14743dfe8a01
MOHAMED Louh <kd_louhbaibat@esi.dz>
2023-12-10 15:57:37 +0100

    version 1 dev 2

--- src/main/java/com/example/service/MatrixMathematics.java
+++ src/main/java/com/example/service/MatrixMathematics.java
@@ -11,13 +11,6 @@ public class MatrixMathematics {
 	private MatrixMathematics(){}
 	
 	
-	/**
-	 * Determinant of a square matrix
-	 * The following function find the determinant in a recursively. 
-	 * @param matrix
-	 * @return
-	 * @throws NoSquareException
-	 */
 	public static double determinant(Matrix matrix) throws NoSquareException {
 		int m = 0;
 		if (!matrix.isSquare())
@@ -36,23 +29,12 @@ public class MatrixMathematics {
 		return sum;
 	}
 
-	/**
-	 * Determine the sign; i.e. even numbers have sign + and odds -
-	 * @param i
-	 * @return
-	 */
 	private static int changeSign(int i) {
 		if (i%2==0)
 			return 1;
 		return -1;
 	}
-	/**
-	 * Creates a submatrix excluding the given row and column
-	 * @param matrix
-	 * @param excluding_row
-	 * @param excluding_col
-	 * @return
-	 */
+
 	public static Matrix createSubMatrix(Matrix matrix, int excluding_row, int excluding_col) {
 		Matrix mat = new Matrix(matrix.getNrows()-1, matrix.getNcols()-1);
 		int r = -1;
@@ -70,20 +52,23 @@ public class MatrixMathematics {
 		return mat;
 	}
 	
-
 	/**
-	 * Transpose of a matrix - Swap the columns with rows
+	 * The cofactor of a matrix
 	 * @param matrix
 	 * @return
+	 * @throws NoSquareException
 	 */
-	public static Matrix transpose(Matrix matrix) {
-		Matrix transposedMatrix = new Matrix(matrix.getNcols(), matrix.getNrows());
+	 
+	
+	public static Matrix cofactor(Matrix matrix) throws NoSquareException {
+		Matrix mat = new Matrix(matrix.getNrows(), matrix.getNcols());
 		for (int i=0;i<matrix.getNrows();i++) {
-			for (int j=0;j<matrix.getNcols();j++) {
-				transposedMatrix.setValueAt(j, i, matrix.getValueAt(i, j));
+			for (int j=0; j<matrix.getNcols();j++) {
+				mat.setValueAt(i, j, changeSign(i) * changeSign(j) * determinant(createSubMatrix(matrix, i, j)));
 			}
 		}
-		return transposedMatrix;
+		
+		return mat;
 	}
 
 
--- src/test/java/MatrixSteps.java
+++ src/test/java/MatrixSteps.java
@@ -14,8 +14,8 @@ import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 public class MatrixSteps {
-    double det ;
-    Matrix transposeMatrix ;
+
+    Matrix cofactorMatrix ;
     Matrix mat;
 
     @Given("I have A Matrix")
@@ -23,32 +23,12 @@ public class MatrixSteps {
          mat=new Matrix();
     }
 
-    @When("I compute determinant of")
-    public void iComputeDeterminantOf(DataTable table) throws NoSquareException {
-        double [][] data = new double[3][3];
-        List<Map<String, Double>> rows = table.asMaps(String.class, Double.class);
-        int i =0;
-        for (Map<String, Double> columns : rows){
-            int j =0;
-            data[i][j]= columns.get("col1");
-            data[i][j+1] = columns.get("col2");
-            data[i][j+2]= columns.get("col3");
-            i=i+1;
-        }
-        mat.setData(data);
-        det = MatrixMathematics.determinant(mat);
-    }
 
-    @Then("The result of determinant is {double}")
-    public void iFindAsDeterminantResult(double arg0) {
-        Assert.assertEquals(arg0,det,0);
+    // Cofactor
 
-    }
-
-
-    @When("I compute transpose of")
-    public void iComputeTransposeOf(DataTable table) throws NoSquareException {
-        double [][] data = new double[3][2];
+    @When("I compute cofactor of")
+    public void iComputeCofactorOf(DataTable table) throws NoSquareException {
+        double [][] data = new double[2][2];
         List<Map<String, Double>> rows = table.asMaps(String.class, Double.class);
         int i =0;
         for (Map<String, Double> columns : rows){
@@ -58,28 +38,25 @@ public class MatrixSteps {
             i=i+1;
         }
         mat.setData(data);
-        transposeMatrix = MatrixMathematics.transpose(mat);
+        cofactorMatrix = MatrixMathematics.cofactor(mat);
     }
 
-    @Then("The result of transpose is")
-    public void iFindAsTransposeResult(DataTable table) {
-        double [][] data = new double[2][3];
+    @Then("The result of cofactor is")
+    public void iFindAsCofactoreResult(DataTable table) {
+        double [][] data = new double[2][2];
         List<Map<String, Double>> rows = table.asMaps(String.class, Double.class);
         int i =0;
         for (Map<String, Double> columns : rows){
             int j =0;
             data[i][j]= columns.get("col1");
             data[i][j+1] = columns.get("col2");
-            data[i][j+2]= columns.get("col3");
             i=i+1;
         }
         Matrix result = new Matrix() ;
         result.setData(data);
-        assertEquals(result,transposeMatrix);
+        assertEquals(result,cofactorMatrix);
 
 
     }
-
-   
 }
 
